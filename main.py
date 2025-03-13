fc7f46ade3620f4759fa7a63cb153a72b5dcc376
yang8249 <63592829+yang8249@users.noreply.github.com>
2025-03-13 19:28:44 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+def read_log_file(filename):
+    try:
+        with open(filename, 'r', encoding='utf-8') as file:
+            for line in file:
+                print(line.strip())
+    except Exception as e:
+        print(f"에러 발생: {e}")
+
+log_file = "mission_computer_main.log"
+read_log_file(log_file)
+
